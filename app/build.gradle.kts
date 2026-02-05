3583687a30a6d89b411ab3a3dbcdf880c789097a
shiraz <Nemez@0990>
2026-02-05 17:06:15 +0500

    Updated Gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.app.githubactionautomation"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.app.githubactionautomation"
